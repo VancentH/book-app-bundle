afa0e688c4e3739ef3584ba4631c919a0eab5018
hyde31456 <hyde31456@gmail.com>
2023-05-26 11:22:30 +0800

    <fix>: update access port number

--- book-app-backend/src/main/java/com/example/bookappbackend/BookAppBackendApplication.java
+++ book-app-backend/src/main/java/com/example/bookappbackend/BookAppBackendApplication.java
@@ -19,7 +19,7 @@ public class BookAppBackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost")
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200")
 						.allowedMethods("GET", "POST", "PUT", "DELETE").allowCredentials(true)
 						.maxAge(3600);
 			}
